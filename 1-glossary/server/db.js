1de61b10b0111e8d6e41d2dcc00d120ef29b9b6b
Mead Solomon <mead93@gmail.com>
2023-06-15 11:23:00 -0500

    sertup mongodb. assigned port

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 
 // 1. Use mongoose to establish a connection to MongoDB
+// ** is the url correct? how do i create a database?
+mongoose.connect('mongodb://localhost/glossaryDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
 // 2. Set up any schema and models needed by the app
+const { Schema } = mongoose;
+
+const glossarySchema = new Schema ({
+  term: String,
+  definition: String
+})
+
+const Glossary = mongoose.model('Glossary', glossarySchema);
+
+// consider making and exporting a save function
+
 // 3. Export the models
+module.exports.Glossary = Glossary;
 // 4. Import the models into any modules that need them
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -2,18 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+
+
+// require statement to import Glossary
+
+
+
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
